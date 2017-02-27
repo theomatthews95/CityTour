1a09eddafa5364d1257448fe425b7f04077ec3b5
Theo Matthews <theo.matthews@hotmail.co.uk>
2017-02-27 23:45:43 +0000

    A test commit

--- app/src/main/java/com/example/admin123/citytour/Fragments/EnterLocationsFragment.java
+++ app/src/main/java/com/example/admin123/citytour/Fragments/EnterLocationsFragment.java
@@ -24,6 +24,7 @@ public class EnterLocationsFragment extends DialogFragment {
         // Empty constructor is required for DialogFragment
         // Make sure not to add arguments to the constructor
         // Use `newInstance` instead as shown below
+        System.out.println("Random thing");
     }
 
     public static EnterLocationsFragment newInstance(String title) {
